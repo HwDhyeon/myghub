51a48d37c104a2846457329ab5934ade5ef0e6b8
HwDhyeon <youfsk@naver.com>
2021-07-12 18:00:10 +0900

    Add GH_TOKEN validate

--- myghub/gh/client.py
+++ myghub/gh/client.py
@@ -2,5 +2,9 @@
 
 from github import Github
 from myghub import env
+from myghub.exceptions import TokenNotFoundError
+
+if not env.GH_TOKEN:
+    raise TokenNotFoundError
 
 github = Github(env.GH_TOKEN)
