8e76fde5bd6876000c46bba0107338f225d027b4
HwDhyeon <youfsk@naver.com>
2021-09-01 10:58:48 +0900

    Add default datetime format for Timemachine

--- myghub/dt/timemachine.py
+++ myghub/dt/timemachine.py
@@ -6,8 +6,11 @@ import datetime
 class TimeMachine:
     """Change String to datetime or datetime to String"""
 
+    def __init__(self, dt_format: str = '%Y-%m-%d %H:%M:%S'):
+        self.format = dt_format
+
     def str_to_dt(
-        self, dt_str: str, dt_format: str = '%Y-%m-%d %H:%M:%S'
+        self, dt_str: str, dt_format: str = None
     ) -> datetime.datetime:
         """Change str to datetime
 
@@ -21,11 +24,14 @@ class TimeMachine:
             datetime.datetime: Datetime object
         """
 
+        if dt_format is None:
+            dt_format = self.format
+
         dt_obj = datetime.datetime.strptime(dt_str, dt_format)
         return dt_obj
 
     def dt_to_str(
-        self, dt_obj: datetime.datetime, dt_format: str = '%Y-%m-%d %H:%M:%S'
+        self, dt_obj: datetime.datetime, dt_format: str = None
     ) -> str:
         """Change datetime to str
 
@@ -38,6 +44,9 @@ class TimeMachine:
         Returns:
             str: Datetime str
         """
+
+        if dt_format is None:
+            dt_format = self.format
 
         dt_str = datetime.datetime.strftime(dt_obj, dt_format)
         return dt_str
